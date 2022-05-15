a6a13e3f945f77073769a27e8ae013d7cafe23e3
Valeria <Lera.lugovskaya@yandex.ru>
2022-05-15 14:49:58 +0300

    first commit

---
+++ calculator/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <CalculatorActions/>
+  </div>
+</template>
+
+<script>
+import CalculatorActions from "@/components/CalculatorActions";
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorActions,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
